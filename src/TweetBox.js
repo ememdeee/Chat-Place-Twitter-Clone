9b1b886b41265bed19ead570e7e07b2f445c21c7
ememdeee <basurah.muhammad.mb@gmail.com>
2023-02-13 13:29:26 +0700

    add tweet massage required function (2)

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -12,19 +12,22 @@ function TweetBox() {
     e.preventDefault();
     // const created = firebase.firestore.Timestamp.fromDate(new Date()).toDate();
     // console.log(created)
-
-    db.collection("postDate").add({
-      displayName : "Mamadun",
-      userName : "iam.meds",
-      verified : false, 
-      text : tweetMessage,
-      avatar : "https://yt3.ggpht.com/yti/AHXOFjX6i-RZRamLO79F8JdKmGOrizu0aH4xLUksIbSbvw=s88-c-k-c0x00ffffff-no-rj-mo",
-      image : tweetImage,
-      date: firebase.firestore.Timestamp.fromDate(new Date()).toDate()
-    });
-
-    setTweetImage("");
-    setTweetMesssage("");
+    if (!tweetMessage) {
+      alert("The tweet message cannot be empty");
+    }
+    else{
+      db.collection("postDate").add({
+        displayName : "Mamadun",
+        userName : "iam.meds",
+        verified : false, 
+        text : tweetMessage,
+        avatar : "https://yt3.ggpht.com/yti/AHXOFjX6i-RZRamLO79F8JdKmGOrizu0aH4xLUksIbSbvw=s88-c-k-c0x00ffffff-no-rj-mo",
+        image : tweetImage,
+        date: firebase.firestore.Timestamp.fromDate(new Date()).toDate()
+      });
+      setTweetImage("");
+      setTweetMesssage("");
+    }
   };
 
   return (
@@ -36,15 +39,7 @@ function TweetBox() {
             </div>
             <input value = {tweetImage} onChange={(e) => setTweetImage(e.target.value)} className='tweetBox_imageInput' placeholder="Optional: Enter image URL" type="text" />
 
-            {/* <Button onClick={sendTweet} type="submit" className='tweetBox_tweetButton'>Tweet</Button> */}
-            <Button onClick={() => {
-              if (!tweetMessage) {
-                alert("The tweet message cannot be empty");
-                return;
-              }
-              sendTweet()
-            }} 
-            type="submit" className='tweetBox_tweetButton'>Tweet</Button>
+            <Button onClick={sendTweet} type="submit" className='tweetBox_tweetButton'>Tweet</Button>
 
         </form>
     </div>
